5f33f1833b0abd2a588ab453ba45a41ea3ae8ae0
Matheus Richard <matheusrichardt@gmail.com>
2020-11-16 13:38:17 -0300

    Remove useless pp

--- src/lit/macros.cr
+++ src/lit/macros.cr
@@ -6,7 +6,6 @@ macro class_property(*property_names)
   {% for prop in property_names %}
     {% prop_name = prop.stringify.split("=").first %}
     @@{{prop}}
-    {% pp prop %}
 
     def self.{{ prop_name }}
       @@{{ prop_name }}
