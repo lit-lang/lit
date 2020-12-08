4e7a9b89bcac063df110223dd01db794138fcc7d
Matheus Richard <matheusrichardt@gmail.com>
2020-12-08 12:55:21 -0300

    Add enclosing to environment

--- spec/lit/environment_spec.cr
+++ spec/lit/environment_spec.cr
@@ -20,6 +20,19 @@ describe Lit::Environment do
 
       env.get(var).should eq true
     end
+
+    context "when variable is on a enclosing scope" do
+      it "searches on the enclosing scope" do
+        enclosing = Lit::Environment.new
+        enclosing.values["my_var"] = true
+        env = Lit::Environment.new(enclosing)
+
+        var = Create.token(:identifier, "my_var")
+
+        env.get(var).should eq true
+      end
+    end
+
     context "when variable does not exist" do
       it "raises an error" do
         env = Lit::Environment.new
@@ -27,6 +40,42 @@ describe Lit::Environment do
 
         expect_raises(Lit::RuntimeError, /Undefined variable 'unknown'./) do
           env.get(var)
+        end
+      end
+    end
+  end
+
+  describe "#assign" do
+    it "changes the value of the given variable" do
+      env = Lit::Environment.new
+      env.values["my_var"] = true
+      var = Create.token(:identifier, "my_var")
+
+      env.assign(var, false)
+
+      env.values["my_var"].should eq false
+    end
+
+    context "when variable is on a enclosing scope" do
+      it "searches on the enclosing scope" do
+        enclosing = Lit::Environment.new
+        enclosing.values["my_var"] = true
+        env = Lit::Environment.new(enclosing)
+        var = Create.token(:identifier, "my_var")
+
+        env.assign(var, false)
+
+        enclosing.values["my_var"].should eq false
+      end
+    end
+
+    context "when variable does not exist" do
+      it "raises an error" do
+        env = Lit::Environment.new
+        var = Create.token(:identifier, "unknown")
+
+        expect_raises(Lit::RuntimeError, /Undefined variable 'unknown'./) do
+          env.assign(var, false)
         end
       end
     end
--- src/lit/environment.cr
+++ src/lit/environment.cr
@@ -7,21 +7,22 @@ module Lit
     getter values
     @values = {} of String => Obj
 
+    def initialize(@enclosing : Environment? = nil); end
+
     def define(name, value)
       @values[name] = value
     end
 
     def get(name : Token)
       return @values[name.lexeme] if @values.has_key? name.lexeme
+      return @enclosing.not_nil!.get(name) if @enclosing
 
       raise RuntimeError.new(name, "Undefined variable '#{name.lexeme}'.")
     end
 
     def assign(name, value) : Nil
-      if @values.has_key? name.lexeme
-        @values[name.lexeme] = value
-        return
-      end
+      return (@values[name.lexeme] = value) if @values.has_key?(name.lexeme)
+      return @enclosing.not_nil!.assign(name, value) if @enclosing
 
       raise RuntimeError.new(name, "Undefined variable '#{name.lexeme}'.")
     end
