941328361b23d7127052f5d59efc965d62b02600
Matheus Richard <matheusrichardt@gmail.com>
2020-12-08 12:56:45 -0300

    Add comments

--- src/lit/environment.cr
+++ src/lit/environment.cr
@@ -15,14 +15,14 @@ module Lit
 
     def get(name : Token)
       return @values[name.lexeme] if @values.has_key? name.lexeme
-      return @enclosing.not_nil!.get(name) if @enclosing
+      return @enclosing.not_nil!.get(name) if @enclosing # TODO: Maybe this could be faster iteratively, not recursively
 
       raise RuntimeError.new(name, "Undefined variable '#{name.lexeme}'.")
     end
 
     def assign(name, value) : Nil
       return (@values[name.lexeme] = value) if @values.has_key?(name.lexeme)
-      return @enclosing.not_nil!.assign(name, value) if @enclosing
+      return @enclosing.not_nil!.assign(name, value) if @enclosing # TODO: Maybe this could be faster iteratively, not recursively
 
       raise RuntimeError.new(name, "Undefined variable '#{name.lexeme}'.")
     end
