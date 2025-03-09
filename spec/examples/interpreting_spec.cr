c48e2e2ed07d03523acb3d4d3344006486b1016b
Matheus Richard <matheusrichardt@gmail.com>
2025-03-09 19:59:01 -0300

    Update example file glob pattern for deeper directory structure

--- spec/examples/interpreting_spec.cr
+++ spec/examples/interpreting_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 describe "Examples" do
   describe "interpreting" do
-    example_files = Dir.glob("examples/tested/*.lit").sort
+    example_files = Dir.glob("examples/tested/**/*.lit").sort
 
     example_files.each do |file|
       it "interprets #{file} correctly" do
